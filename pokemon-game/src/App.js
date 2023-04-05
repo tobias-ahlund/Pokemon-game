15ea8d0b58adbdb4078693f06895688958c415c0
gustavenoksson <gustav.enoksson@live.se>
2023-04-05 10:38:34 +0200

    Added fetch to pokemon API

--- pokemon-game/src/App.js
+++ pokemon-game/src/App.js
@@ -2,22 +2,17 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const randomId = Math.ceil(Math.random() * 151);
+
+  const url = `https://pokeapi.co/api/v2/pokemon/${randomId}`;
+
+  fetch(url)
+  .then((res) => res.json())
+  .then((json => console.log(json.forms[0].name)));
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
     </div>
   );
 }
