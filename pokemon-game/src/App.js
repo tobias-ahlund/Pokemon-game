9d55286d600a7bc72bbd51c248f821ecde1a0c3d
tobias-ahlund <tobias.ahlund@gmail.com>
2023-04-12 09:15:23 +0200

    added in-game window

--- pokemon-game/src/App.js
+++ pokemon-game/src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
+import GameWindow from "./components/GameWindow";
 import Pokemon from "./components/Pokemon";
 
 function App() {
   return (
     <div className="App">
-      <Pokemon />
+      <GameWindow>
+        <Pokemon />
+      </GameWindow>
     </div>
   );
 }
---
+++ pokemon-game/src/components/GameWindow/index.js
@@ -0,0 +1,11 @@
+import "./GameWindow.css";
+
+const GameWindow = () => {
+    return (
+        <div>
+            <img class="background-image" src="../../assets/images/background-image.jpg" alt="Environment background"></img>
+        </div>
+    );
+};
+
+export default GameWindow;
