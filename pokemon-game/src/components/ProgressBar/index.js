2fa9f5dc38ba153f86fb7f401fa2f1995e20457f
gustavenoksson <gustav.enoksson@live.se>
2023-05-02 11:27:42 +0200

    experience now works with the progress bar being updated, level also updated

--- pokemon-game/src/components/ProgressBar/index.js
+++ pokemon-game/src/components/ProgressBar/index.js
@@ -4,18 +4,35 @@ import { useEffect, useState } from 'react';
 const ProgressBar = (props) => {
 
     const [experience, setExperience] = useState(0);
+    const [max, setMax] = useState(100);
+    const [level, setLevel] = useState(0);
 
     useEffect(() => {
         setInterval(() => {
-            let localExperience = parseInt(localStorage.getItem('experience') || 0);
-            setExperience(localExperience);
+            setExperience(localStorage.getItem('experience'));
         }, 1000);
-    }, []);
+
+        let localLevel = parseInt(localStorage.getItem('level') || 0);
+        const progress = document.getElementById("progress");
+
+        if(experience >= progress.max) {
+            let updatedLevel = localLevel + 1;
+            localStorage.setItem('level', updatedLevel);
+            localStorage.setItem('experience', 0);
+            setLevel(updatedLevel);
+            setMax(max * 1.5);
+        }
+
+        setLevel(parseInt(localStorage.getItem('level')) || 0);
+        setExperience(localStorage.getItem('experience'));
+
+    }, [experience]);
 
     return(
         <div className={styles.xp_bar_container}>
-            <label for="xp"><b>Experience:</b> {experience} / 250</label>
-            <progress id="xp" className={styles.progress} value={experience} max="250">Hello</progress>
+            <p className={styles.level}>Level: {level}</p>
+            <label for="progress"><b>Experience:</b> {experience} / {max}</label>
+            <progress id="progress" className={styles.progress} value={experience} max={max}>Hello</progress>
         </div>
     )
 }
